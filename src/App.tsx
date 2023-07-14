887cde389f4cbab415ed79b0155248e0da7dff58
Tasnim Pricila <tasnimtanzim55@gmail.com>
2023-07-14 22:20:55 +0600

    header design added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,45 @@
-import { Button } from 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 function App() {
-
   return (
     <>
-      <Button variant="primary">Primary</Button>
+      <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav ">
+            <Nav className="me-auto w-50 d-flex justify-content-end">
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2 border border-primary"
+                  aria-label="Search"
+                />
+                <Button variant="outline-primary">Search</Button>
+              </Form>
+            </Nav>
+            <Nav className=""
+            // className='d-flex justify-content-end w-100'
+            >
+              <Nav.Link href="#deets">All Books</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                Sign In
+              </Nav.Link>
+              <Nav.Link eventKey={3} href="#memes">
+                Sign Up
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
