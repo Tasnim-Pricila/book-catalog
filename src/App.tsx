f602451fdfa7a024edfc440304fd26206fcc3d68
Tasnim Pricila <tasnimtanzim55@gmail.com>
2023-07-14 18:32:34 +0600

    initial setup done

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <>
+      <Button variant="primary">Primary</Button>
+    </>
+  )
+}
+
+export default App
