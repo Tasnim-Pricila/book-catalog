984eba24dd92adc36a72600d0287e794098b2dcc
Tasnim Pricila <tasnimtanzim55@gmail.com>
2023-08-01 22:48:21 +0600

    server link added

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1",
+    baseUrl: "https://book-catalog-server-rouge.vercel.app/api/v1",
   }),
   tagTypes: ["comment", "wishlist", "create", "delete"],
 
