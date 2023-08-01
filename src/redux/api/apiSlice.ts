60b73ee95baf9c5dbde658b4766e60765565f284
Tasnim Pricila <tasnimtanzim55@gmail.com>
2023-08-02 00:45:28 +0600

    link changed

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://book-catalog-server-rouge.vercel.app/api/v1",
+    baseUrl: "https://book-catalog-server-tasnim-pricila.vercel.app/api/v1",
   }),
   tagTypes: ["comment", "wishlist", "create", "delete"],
 
